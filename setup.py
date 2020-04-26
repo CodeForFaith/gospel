d5d470b07b1b05329e60aeb6a5fdacda68c009a4
CodeForFaith <contact@codeforfaith.com>
2020-04-26 18:15:13 +0200

    improvement(.): update version to 0.0.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="gospel",
-    version="0.0.1",
+    version="0.0.3",
     description="The Gospel of Jesus Christ",
     long_description=README,
     long_description_content_type="text/markdown",
